cfae571169d7d4c604a6a5969826f9b37223c1c5
Ernesto Valenzuela Vargas <hello@netov.dev>
2020-05-02 15:49:44 -0500

    Curso interfaces con python, agregar tablas y dividir box de toga

--- Python/InterfacesPython/app/main.py
+++ Python/InterfacesPython/app/main.py
@@ -8,16 +8,37 @@ class PokeDex(toga.App):
         self.title = title
         self.size = (WIDTH, HEIGHT)
 
+        self.heading = ['Name']
+        self.data = [ 'Python', 'Ruby', 'Laravel', 'Sql' ]
+
+        self.create_elements()
 
     def startup(self):
         self.main_window = toga.MainWindow('main', title=self.title, size=self.size)
 
         box = toga.Box()
-        self.main_window.content = box
+
+        ##Agregar tabla al contenedor
+        #box.add(self.table)
+
+        # Dividir ventana
+        split = toga.SplitContainer()
+        split.content = [self.table, box]
+
+        self.main_window.content = split
         self.main_window.show()
 
     def create_elements(self):
-        pass
+        self.create_table()
+
+    def create_table(self):
+        self.table = toga.Table(self.heading, data=self.data, on_select=self.select_element)
+
+    #callbacks
+    def select_element(self, widget, row):
+        if row:
+            print(row.name)
+
 
 if __name__ == '__main__':
     pokedex = PokeDex('PokeDex', 'dev.netov.PokeDex')
